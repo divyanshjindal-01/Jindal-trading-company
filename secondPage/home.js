6a1db4f3cf2a21459069ed157222e545a3c06e48
Divyanshu Jindal. <divyanshjindal.05@gmail.com.>
2025-01-17 01:15:28 +0530

    work on home page

--- secondPage/home.js
+++ secondPage/home.js
@@ -1,2 +1,15 @@
 let itemName =  document.querySelectorAll("#itemName");
-let search = document.querySelector("#searchBox");
+let searchBar = document.getElementById("searchBox");
+const item = document.querySelectorAll(".itemName");
+
+searchBar.addEventListener('input',()=>{
+    const searchValue = searchBar.value.toLowerCase();
+
+    item.forEach(item =>{
+        if(item.textContent.toLowerCase().includes(searchValue)){
+            item.classList.add('highlight');
+        }else{
+            item.classList.remove('highlight');
+        }
+    });
+});
